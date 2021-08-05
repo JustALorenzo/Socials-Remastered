9053d7ac2b7f865396bcae8ec8dda8ff8f13c76e
TheRealBeaver <36443552+TheRealBeaver@users.noreply.github.com>
2021-08-05 22:54:40 +0200

    setup for commands

--- src/main/java/me/justalorenzo/socials/Main.java
+++ src/main/java/me/justalorenzo/socials/Main.java
@@ -2,9 +2,12 @@ package me.justalorenzo.socials;
 
 
 import com.google.inject.Injector;
+import me.justalorenzo.socials.commands.CommandHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    CommandHandler commands = new CommandHandler();
 
     public void onEnable() {
         this.getLogger().info("Socials Started");
@@ -12,7 +15,7 @@ public class Main extends JavaPlugin {
         SimpleBinderModule injectThisClass = new SimpleBinderModule(this);
         Injector injector = injectThisClass.createInjector();
         injector.injectMembers(this);
-
+        this.getCommand("discord").setExecutor(commands);
 
     }
     public void onDisable() {
--- src/main/java/me/justalorenzo/socials/commands/CommandHandler.java
+++ src/main/java/me/justalorenzo/socials/commands/CommandHandler.java
@@ -1,12 +1,24 @@
 package me.justalorenzo.socials.commands;
 
+import com.google.inject.Inject;
+import me.justalorenzo.socials.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class CommandHandler implements CommandExecutor {
+
+
+String[] commands = {"Discord"};
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        for(String cmd : commands) {
+            if(command.getName().equalsIgnoreCase(cmd)) {
+                commandSender.sendMessage("wow you did a " + command.getName());
+                return true;
+            }
+        }
         return false;
     }
 }
