b5eefd8e5ec6f15ccf940a0f481763856b3671ff
TheRealBeaver <36443552+TheRealBeaver@users.noreply.github.com>
2021-08-05 22:47:37 +0200

    add necessary steps for maven to work.

--- src/main/java/me/justalorenzo/socials/commands/CommandHandler.java
+++ src/main/java/me/justalorenzo/socials/commands/CommandHandler.java
@@ -1,4 +1,12 @@
 package me.justalorenzo.socials.commands;
 
-public class CommandHandler {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandHandler implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
 }
