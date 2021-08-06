3cf45941de991cd9f276faeb8e22ceeb8a87c352
TheRealBeaver <36443552+TheRealBeaver@users.noreply.github.com>
2021-08-06 10:56:14 +0200

    committing berfore i mess up config commands

--- src/main/java/me/justalorenzo/socials/commands/ConfigCommands.java
+++ src/main/java/me/justalorenzo/socials/commands/ConfigCommands.java
@@ -1,20 +1,41 @@
 package me.justalorenzo.socials.commands;
 
+import com.google.inject.Inject;
+import me.justalorenzo.socials.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class ConfigCommands implements CommandExecutor {
+
+    @Inject
+    private Main plugin;
+
+    boolean foundSocials;
+
     //modify config lines
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        String[] commands = Main.commandLinkList;
+        if (command.getName().equalsIgnoreCase("set")) {
 
-        if(command.getName().equalsIgnoreCase("set")) {
-            if(args.length < 2) {
+            if (args.length < 2) {
                 commandSender.sendMessage("You must specify a social platform and link! /set youtube <link> for example!");
-            }
-            else {
-                commandSender.sendMessage("ok working on it");
+            } else {
+                foundSocials = false;
+                for (String cmd : commands) {
+                    if (command.getName().equalsIgnoreCase(cmd)) {
+                        foundSocials = true;
+                    }
+                }
+                if (foundSocials) {
+                    //go to config & edit it
+                } else {
+                    //tell them we couldn't find  args[1]
+                }
+
+
+
             }
             return true;
         }
