f1848b5b06d171720b6d83b9a859b51ea063b807
TheRealBeaver <36443552+TheRealBeaver@users.noreply.github.com>
2021-08-06 20:16:29 +0200

    permission addition

--- src/main/java/me/justalorenzo/socials/Socials.java
+++ src/main/java/me/justalorenzo/socials/Socials.java
@@ -6,14 +6,16 @@ import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import me.justalorenzo.socials.commands.CommandHandler;
 import me.justalorenzo.socials.commands.ConfigCommands;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Singleton
 public class Socials extends JavaPlugin {
 
 
-    public static final String[] COMMAND_LINK_LIST = {"youtube", "facebook", "twitter", "tiktok", "discord", "twitch", "github", "spotify", "steam", "reddit", "instagram"};
-    public static final String SET_COMMAND = "set";
+    public static final String[] COMMAND_LINK_LIST = {"youtube", "facebook", "twitter", "tiktok", "discord", "twitch", "github", "spotify", "steam", "reddit", "instagram", "linktree"};
+    public static final String SET_COMMAND = "setlink";
     @Inject private ConfigCommands configCommands;
     @Inject private CommandHandler commandHandler;
     public void onEnable() {
@@ -40,4 +42,12 @@ public class Socials extends JavaPlugin {
         this.getLogger().info("Socials Terminated");
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("about")) {
+            sender.sendMessage("Lightweight optimized & open source plugin to list your socials!");
+            sender.sendMessage("Made by https://github.com/TheRealBeaver");
+        }
+        return true;
+    }
 }
--- src/main/java/me/justalorenzo/socials/commands/ConfigCommands.java
+++ src/main/java/me/justalorenzo/socials/commands/ConfigCommands.java
@@ -3,6 +3,7 @@ package me.justalorenzo.socials.commands;
 import com.google.inject.Inject;
 import me.justalorenzo.socials.Socials;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,45 +21,49 @@ public class ConfigCommands implements CommandExecutor {
 
     boolean foundSocials;
 
-    //temporary before injection fix
+    //importing lists of commands, so they're global and, they don't have to be set individually for each update
     String[] commandList = Socials.COMMAND_LINK_LIST;
-
+    String setCommand = Socials.SET_COMMAND;
 
     //modify config lines
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
 
 
-        if (command.getName().equalsIgnoreCase("set")) {
-
-            if (args.length < 2) {
-                commandSender.sendMessage("You must specify a social platform and link! /set youtube <link> for example!");
-            } else {
-                foundSocials = false;
-                for (String cmd : commandList) {
-                    if (args[0].equalsIgnoreCase(cmd)) {
-                        foundSocials = true;
-                        break;
-                    }
-                }
-                if (foundSocials) {
-                    //go to config & edit it
-                    if (plugin == null) {
-                        commandSender.sendMessage("Plugin instance is null...");
-                    } else {
-                        plugin.getConfig().set(args[0], args[1]);
-                        plugin.saveConfig();
-                        plugin.getLogger().info("Saved it!");
-                    }
-
+        if (command.getName().equalsIgnoreCase(setCommand)) {
+            if (commandSender.hasPermission("socials.set")) {
+                if (args.length < 2) {
+                    commandSender.sendMessage("You must specify a social platform and link! /set youtube <link> for example!");
                 } else {
-                    //tell them we couldn't find  args[1]
-                    commandSender.sendMessage("We do not support " + args[0] + " as of now, please suggest it if you want it added!");
+                    foundSocials = false;
+                    for (String cmd : commandList) {
+                        if (args[0].equalsIgnoreCase(cmd)) {
+                            foundSocials = true;
+                            break;
+                        }
+                    }
+                    if (foundSocials) {
+                        //go to config & edit it
+                        if (plugin == null) {
+                            commandSender.sendMessage("Plugin instance is null...");
+                        } else {
+                            plugin.getConfig().set(args[0], args[1]);
+                            plugin.saveConfig();
+                            plugin.getLogger().info("Saved it!");
+                        }
+
+                    } else {
+                        //tell them we couldn't find  args[1]
+                        commandSender.sendMessage(ChatColor.BLUE + "We do not support " + args[0] + " as of now, please suggest it if you want it added!");
+                    }
+
+
                 }
 
-
+            } else {
+                commandSender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+                return true;
             }
-
         }
         return true;
     }
