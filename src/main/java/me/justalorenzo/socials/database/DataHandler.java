a7848a95198418c68aaf14c9d0363546795d5471
TheRealBeaver <36443552+TheRealBeaver@users.noreply.github.com>
2021-08-12 14:27:46 +0200

    fixing extra comma

--- src/main/java/me/justalorenzo/socials/database/DataHandler.java
+++ src/main/java/me/justalorenzo/socials/database/DataHandler.java
@@ -18,7 +18,7 @@ public class DataHandler {
         String link = args[1];
 
         try {
-            PreparedStatement data = connection.prepareStatement("INSERT INTO socials (platform, link, customhead) VALUES ('" + platform + "', '" + link + "', 'eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTY3ZDgxM2FlN2ZmZTViZTk1MWE0ZjQxZjJhYTYxOWE1ZTM4OTRlODVlYTVkNDk4NmY4NDk0OWM2M2Q3NjcyZSJ9fX0=',)");
+            PreparedStatement data = connection.prepareStatement("INSERT INTO socials (platform, link, customhead) VALUES ('" + platform + "', '" + link + "', 'eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTY3ZDgxM2FlN2ZmZTViZTk1MWE0ZjQxZjJhYTYxOWE1ZTM4OTRlODVlYTVkNDk4NmY4NDk0OWM2M2Q3NjcyZSJ9fX0=')");
             data.executeUpdate();
         } catch (Exception e) {
     e.printStackTrace();
